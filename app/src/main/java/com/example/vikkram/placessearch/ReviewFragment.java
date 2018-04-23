491673f8104b6ef32ae178629e0b15c4d1ddbb67
Vikkram Vasuki <vikkram1995.vasuki@gmail.com>
2018-04-22 21:14:09 -0700

    Add json parsing code for making yelp api request

--- app/src/main/java/com/example/vikkram/placessearch/ReviewFragment.java
+++ app/src/main/java/com/example/vikkram/placessearch/ReviewFragment.java
@@ -1,6 +1,7 @@
 package com.example.vikkram.placessearch;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,6 +14,13 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,6 +36,8 @@ public class ReviewFragment extends android.support.v4.app.Fragment implements A
     private static ReviewAdapter adapter;
     ArrayList<JSONObject> google_reviews;
     ArrayList<JSONObject> default_google_reviews;
+    ArrayList<JSONObject> yelp_reviews;
+    ArrayList<JSONObject> default_yelp_reviews;
 
     RecyclerView recyclerView;
     View view;
@@ -41,6 +51,8 @@ public class ReviewFragment extends android.support.v4.app.Fragment implements A
         default_google_reviews= new ArrayList<>();
         getDefaultReviews(true);
         default_google_reviews.addAll(google_reviews);
+        getYelpReviews();
+
         /*
         JSONArray reviewData= new JSONArray();
         try {
@@ -94,6 +106,115 @@ public class ReviewFragment extends android.support.v4.app.Fragment implements A
         return view;
     }
 
+
+    public void getYelpReviews()
+    {
+
+        String url="";
+        String country="";
+        String state= "";
+        String city="";
+
+
+        JSONObject json= ((PlacesInfo)this.getActivity()).js;
+        Log.d(TAG, "getYelpReviews: "+json);
+        JSONObject results = null;
+        try {
+            results = json.getJSONObject("result");
+           // Log.d(TAG, "getYelpReviews: "+results);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JSONArray address_components = null;
+        ArrayList<JSONObject> address;
+        if(results != null)
+        {
+            //Log.d(TAG, "getYelpReviews: "+results);
+            try {
+                Log.d(TAG, "get Address components: "+ results.get("address_components"));
+                address_components= results.getJSONArray("address_components");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for(int i=0;i<address_components.length();++i)
+        {
+            JSONObject innerDet= null;
+            JSONArray types= null;
+            try {
+                innerDet = address_components.getJSONObject(i);
+
+                types= innerDet.getJSONArray("types");
+                Log.d(TAG, "Types of places:  "+types);
+
+                for(int j=0;j<types.length();++j)
+                {
+                    if(types.getString(j).equals("country"))
+                    {
+                        country = innerDet.getString("short_name");
+                    }
+                    else if(types.getString(j).equals("administrative_area_level_1"))
+                    {
+                        state = innerDet.getString("short_name");
+                    }
+                    else if(types.getString(j).equals("locality"))
+                    {
+                        city= innerDet.getString("short_name");
+                    }
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+
+        }
+
+        Log.d(TAG, "Country: "+country+" State: "+state+" City: "+city);
+
+
+        //Log.d(TAG, "getYelpReviews: ");
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,
+                url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                try {
+                    Log.d(TAG, "Submitted results"+response);
+                    JSONObject jsonObject = new JSONObject(response);
+                    //Toast.makeText(getContext(), "No Error", Toast.LENGTH_SHORT).show();
+                    JSONArray array = jsonObject.getJSONArray("results");
+
+                    for (int i = 0; i < array.length(); ++i) {
+                        JSONObject res = array.getJSONObject(i);
+
+                    }
+
+
+
+                } catch (JSONException e) {
+
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Toast.makeText(getContext(), "Error" + error.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(stringRequest);
+        
+
+    }
 
     public void getDefaultReviews(boolean type)
     {
