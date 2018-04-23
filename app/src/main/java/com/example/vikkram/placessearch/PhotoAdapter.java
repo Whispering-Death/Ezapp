73b722a083425b892ce9030bc6ffc2b203ca82da
Vikkram Vasuki <vikkram1995.vasuki@gmail.com>
2018-04-23 08:48:24 -0700

    Add photo fragment and adapter code

---
+++ app/src/main/java/com/example/vikkram/placessearch/PhotoAdapter.java
@@ -0,0 +1,105 @@
+package com.example.vikkram.placessearch;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.PlacePhotoMetadata;
+import com.google.android.gms.location.places.PlacePhotoResponse;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.tasks.Task;
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class PhotoAdapter extends RecyclerView.Adapter<PhotoAdapter.ViewHolder> {
+
+    private List<PlacePhotoMetadata> mData;
+    private LayoutInflater mInflater;
+    private GeoDataClient geoDataClient;
+    private PhotoAdapter.ItemClickListener mClickListener;
+    private static final String TAG = "PhotoAdapter";
+    View view;
+    SharedPreferences myprefs;
+    // data is passed into the constructor
+    PhotoAdapter(Context context, List<PlacePhotoMetadata> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+        Log.d(TAG, "PhotoAdapter: "+this.mData.size());
+    }
+
+    @NonNull
+    @Override
+    public PhotoAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        geoDataClient = Places.getGeoDataClient(parent.getContext(),null);
+        view = mInflater.inflate(R.layout.photo_fragment, parent, false);
+
+        return  new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PhotoAdapter.ViewHolder holder, int position) {
+        Task<PlacePhotoResponse> photoResponse = geoDataClient.getPhoto(mData.get(position));
+        PlacePhotoResponse photo = photoResponse.getResult();
+        Bitmap photoBitmap = photo.getBitmap();
+        holder.myPhoto.setImageBitmap(photoBitmap);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        ImageView myPhoto;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            myPhoto= itemView.findViewById(R.id.photos);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    // convenience method for getting data at click position
+    /*JSONObject getItem(int id) {
+        return mData.get(id);
+    }*/
+    private void toastMessage(String message)
+    {
+
+        Toast t = Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT);
+        //View v1 = t.getView();
+        //v1.setBackgroundColor(Integer.parseInt("#cccccc"));
+        t.show();
+
+        //Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+    }
+    // allows clicks events to be caught
+    void setClickListener(PhotoAdapter.ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+}
--- app/src/main/java/com/example/vikkram/placessearch/PhotoFragment.java
+++ app/src/main/java/com/example/vikkram/placessearch/PhotoFragment.java
@@ -1,20 +1,101 @@
 package com.example.vikkram.placessearch;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.PlacePhotoMetadata;
+import com.google.android.gms.location.places.PlacePhotoMetadataBuffer;
+import com.google.android.gms.location.places.PlacePhotoMetadataResponse;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PhotoFragment extends android.support.v4.app.Fragment{
 
     private static final String TAG = "PhotoFragment";
+    private GeoDataClient geoDataClient;
+    private int currentPhotoIndex = 0;
+    private List<PlacePhotoMetadata> photosDataList;
+    private static PhotoAdapter adapter;
+
     View view;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.photo_fragment, container, false);
+        view = inflater.inflate(R.layout.photo_adapter, container, false);
+
+
+        geoDataClient = Places.getGeoDataClient(getContext(),null);
+        JSONObject json= ((PlacesInfo)this.getActivity()).js;
+        photosDataList = new ArrayList<>();
+        try {
+            String placeid=json.getJSONObject("result").getString("place_id");
+            getPhotoMetadata(placeid);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+       // Log.d(TAG, "Place metadata: "+photosDataList.size());
+        RecyclerView recyclerView = view.findViewById(R.id.photo_recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new PhotoAdapter(getContext(), photosDataList);
+        //adapter.setClickListener(this);
+
+        //recyclerView.invalidate();
+        recyclerView.setAdapter(adapter);
         return view;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+    }
+
+    private void getPhotoMetadata(String placeId) {
+
+        final Task<PlacePhotoMetadataResponse> photoResponse =
+                geoDataClient.getPlacePhotos(placeId);
+
+        photoResponse.addOnCompleteListener
+                (new OnCompleteListener<PlacePhotoMetadataResponse>() {
+                    @Override
+                    public void onComplete(@NonNull Task<PlacePhotoMetadataResponse> task) {
+                        currentPhotoIndex = 0;
+
+                        PlacePhotoMetadataResponse photos = task.getResult();
+                        PlacePhotoMetadataBuffer photoMetadataBuffer = photos.getPhotoMetadata();
+
+                        Log.d(TAG, "number of photos "+photoMetadataBuffer.getCount());
+
+                        for(PlacePhotoMetadata photoMetadata : photoMetadataBuffer){
+                            photosDataList.add(photoMetadataBuffer.get(0).freeze());
+                            Log.d(TAG, "added ");
+                        }
+
+                        photoMetadataBuffer.release();
+                        //displayPhoto();
+                    }
+                });
+        //Log.d(TAG, "Place metadata: "+photosDataList.size());
+    }
+
 }
