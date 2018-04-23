cb181925dc672c5c0899ec02e467b941d0d592d8
Vikkram Vasuki <vikkram1995.vasuki@gmail.com>
2018-04-23 01:48:34 -0700

    Add map fragment code

--- app/src/main/java/com/example/vikkram/placessearch/MapFragment.java
+++ app/src/main/java/com/example/vikkram/placessearch/MapFragment.java
@@ -2,6 +2,7 @@ package com.example.vikkram.placessearch;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -29,15 +31,19 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.PolyUtil;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.widget.Toast.*;
 
 import java.net.URLEncoder;
+import java.util.Map;
 
-public class MapFragment extends android.support.v4.app.Fragment implements OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener{
+public class MapFragment extends android.support.v4.app.Fragment implements OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener, AdapterView.OnItemSelectedListener{
     private static final String TAG = "MapFragment";
 
     private GoogleApiClient mGoogleApiClient;
@@ -48,6 +54,7 @@ public class MapFragment extends android.support.v4.app.Fragment implements OnMa
     private double lat = 0.0;
     private double lng=0.0;
 
+    GoogleMap mainMap;
     View view;
     @Nullable
     @Override
@@ -85,12 +92,17 @@ public class MapFragment extends android.support.v4.app.Fragment implements OnMa
         final AutoCompleteTextView atv = (AutoCompleteTextView)view.findViewById(R.id.map_atv_destination);
         atv.setAdapter(mPlaceAutocompleteAdapter);
 
+        Spinner spn = (Spinner)view.findViewById(R.id.spinnerCategory);
+
+        spn.setOnItemSelectedListener(this);
         atv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Log.d(TAG, "Item clicked: "+atv.getText().toString());
                 Log.d(TAG, "Calling directions api");
-               getDirections();
+
+
+                getDirections("driving");
                 //String url = "http://my-cloned-env-vasuki.us-west-2.elasticbeanstalk.com/placedetails?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4";
 
 
@@ -102,7 +114,7 @@ public class MapFragment extends android.support.v4.app.Fragment implements OnMa
     }
 
 
-    public void getDirections()
+    public void getDirections(String mode)
     {
 
         Log.d(TAG, "Making google directions api request");
@@ -110,8 +122,10 @@ public class MapFragment extends android.support.v4.app.Fragment implements OnMa
         AutoCompleteTextView dst = (AutoCompleteTextView)view.findViewById(R.id.map_atv_destination);
         String source = dst.getText().toString();
         String destination= String.valueOf(lat)+","+String.valueOf(lng);
-        String url = "https://maps.googleapis.com/maps/api/directions/json?origin="+ URLEncoder.encode(source)+"&destination="+destination+"&key=AIzaSyAYKYTAwBVMKXjmcfhD-EGLkhbL-9Yxayg";
+        Log.d(TAG, "Mode of travel: "+mode);
+        String url = "https://maps.googleapis.com/maps/api/directions/json?origin="+ URLEncoder.encode(source)+"&destination="+destination+"&mode="+mode+"&key=AIzaSyAYKYTAwBVMKXjmcfhD-EGLkhbL-9Yxayg";
 
+        mainMap.clear();
         StringRequest stringRequest = new StringRequest(Request.Method.GET,
                 url, new Response.Listener<String>() {
 
@@ -124,9 +138,13 @@ public class MapFragment extends android.support.v4.app.Fragment implements OnMa
                 try {
                     Log.d(TAG, "Submitted results"+response);
                     JSONObject jsonObject = new JSONObject(response);
+                    JSONArray jsonArray = jsonObject.getJSONArray("routes").getJSONObject(0).getJSONArray("legs").getJSONObject(0).getJSONArray("steps");
+                    Log.d(TAG, "onResponse: "+jsonArray);
 
-                    //Log.d(TAG, "onResponse: "+jsonObject.toString());
-
+                    String[] points;
+                    points= getPaths(jsonArray);
+                    displayDirection(points);
+                    //Log.d(TAG, "Polyline points obtained: "+points);
 
 
 
@@ -145,22 +163,85 @@ public class MapFragment extends android.support.v4.app.Fragment implements OnMa
         });
         RequestQueue requestQueue = Volley.newRequestQueue(getContext());
         requestQueue.add(stringRequest);
+
+
+        //Log.d(TAG, "Polyline point list: "+points);
     }
+
+    public String[] getPaths(JSONArray googlesteps)
+    {
+        int count = googlesteps.length();
+        String[] polylines = new String[count];
+
+        for(int i = 0;i<count;i++)
+        {
+            try {
+                polylines[i] = getPath(googlesteps.getJSONObject(i));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return polylines;
+    }
+
+
+    public void displayDirection(String[] directionsList)
+    {
+
+        int count = directionsList.length;
+        for(int i = 0;i<count;i++)
+        {
+            PolylineOptions options = new PolylineOptions();
+            options.color(Color.BLUE);
+            options.width(10);
+            options.addAll(PolyUtil.decode(directionsList[i]));
+
+            mainMap.addPolyline(options);
+        }
+    }
+    public String getPath(JSONObject googlePathJson)
+    {
+        String polyline = "";
+        try {
+            polyline = googlePathJson.getJSONObject("polyline").getString("points");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return polyline;
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         LatLng dest = new LatLng(lat, lng);
 
 
-        
+        mainMap = googleMap;
 
         googleMap.addMarker(new MarkerOptions().position(dest)
                 .title("Marker in Sydney"));
+
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(dest));
+        googleMap.animateCamera( CameraUpdateFactory.zoomTo( 17.0f ) );
 
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        String item= parent.getItemAtPosition(position).toString();
+
+        Log.d(TAG, "onItemSelected: "+item);
+        getDirections(item.toLowerCase());
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
